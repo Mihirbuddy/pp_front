02f7fb57306bbaad30b9cfe0cde4ff95587db34a
Mihirbuddy <100517418+Mihirbuddy@users.noreply.github.com>
2024-03-28 19:56:34 +0530

    Update Login.js

--- src/users/pages/Login.js
+++ src/users/pages/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     setError(null);
     try {
       const response = await fetch(
-        "https://backend-cv5pyz7l0-mihirbuddys-projects.vercel.app/api/users/login",
+        "https://backend-delta-dusky.vercel.app/api/users/login",
         {
           //const response = await fetch("http://localhost:5000/api/users/login", {
           method: "POST",
